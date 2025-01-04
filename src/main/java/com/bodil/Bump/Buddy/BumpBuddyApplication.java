e7eb576ed6c93dae8ab3b04965cf399ad0ff3fa1
Bobbie Janssen <174934975+BobbieJanssen@users.noreply.github.com>
2025-01-04 11:58:27 +0100

    Initial Commit

---
+++ src/main/java/com/bodil/Bump/Buddy/BumpBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.bodil.Bump.Buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumpBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumpBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bodil/Bump/Buddy/BumpBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodil.Bump.Buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumpBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
