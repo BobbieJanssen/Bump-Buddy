1ca7d4ed2f61abd610fd0f1923bbefda836a33ba
Bobbie Janssen <174934975+BobbieJanssen@users.noreply.github.com>
2025-01-30 14:36:33 +0100

    PhotoalbumService ingevuld en bij de repository findAllByUserId toegevoegd

--- src/main/java/com/bodil/Bump/Buddy/service/implementaties/PhotoalbumServiceImp.java
+++ src/main/java/com/bodil/Bump/Buddy/service/implementaties/PhotoalbumServiceImp.java
@@ -1,4 +1,52 @@
 package com.bodil.Bump.Buddy.service.implementaties;
 
-public class PhotoalbumServiceImp {
+import com.bodil.Bump.Buddy.model.Photoalbum;
+import com.bodil.Bump.Buddy.repository.interfaces.PhotoalbumRepository;
+import com.bodil.Bump.Buddy.service.interfaces.PhotoalbumService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PhotoalbumServiceImp implements PhotoalbumService {
+    private final PhotoalbumRepository photoalbumRepository;
+
+    public PhotoalbumServiceImp(PhotoalbumRepository photoalbumRepository) {
+        this.photoalbumRepository = photoalbumRepository;
+    }
+
+    @Override
+    public List<Photoalbum> getAllPhotoalbums() {
+        return photoalbumRepository.findAll();
+    }
+
+    @Override
+    public Optional<Photoalbum> getPhotoalbumById(long id) {
+        return photoalbumRepository.findById(id);
+    }
+
+    @Override
+    public List<Photoalbum> findAllByUserId(long userId) {
+        return photoalbumRepository.findAllByUserId(userId);
+    }
+
+    @Override
+    public Photoalbum createPhotoalbum(Photoalbum photoalbum) {
+        return photoalbumRepository.save(photoalbum);
+    }
+
+    @Override
+    public Photoalbum updatePhotoalbum(long id, Photoalbum photoalbum) {
+        if (photoalbumRepository.existsById(id)) {
+            return photoalbumRepository.save(photoalbum);
+        } else {
+            throw new RuntimeException("Photoalbum not found");
+        }
+    }
+
+    @Override
+    public void deletePhotoalbum(long id) {
+        photoalbumRepository.deleteById(id);
+    }
 }
--- src/main/java/com/bodil/Bump/Buddy/service/interfaces/PhotoalbumService.java
+++ src/main/java/com/bodil/Bump/Buddy/service/interfaces/PhotoalbumService.java
@@ -1,4 +1,15 @@
 package com.bodil.Bump.Buddy.service.interfaces;
 
+import com.bodil.Bump.Buddy.model.Photoalbum;
+import java.util.List;
+import java.util.Optional;
+
 public interface PhotoalbumService {
+    List<Photoalbum> getAllPhotoalbums();
+    Optional<Photoalbum> getPhotoalbumById(long id);
+    List<Photoalbum> findAllByUserId(long userId);
+    Photoalbum createPhotoalbum(Photoalbum photoalbum);
+    Photoalbum updatePhotoalbum(long id, Photoalbum photoalbum);
+    void deletePhotoalbum(long id);
 }
+
